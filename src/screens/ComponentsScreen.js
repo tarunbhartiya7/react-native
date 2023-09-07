4934164af34ec01a0ba244cfcaf010eb6f5ec72b
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2023-09-07 23:47:57 +0530

    second module

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    color: "cyan",
+    backgroundColor: "#454545",
+  },
+});
+
+const greeting = "Getting started with React Native!";
+const name = "Samoa";
+const message = <Text style={styles.text}>My name is {name}</Text>;
+
+const ComponentsScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 45 }}>{greeting}</Text>
+      {message}
+    </View>
+  );
+};
+
+export default ComponentsScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>Hi Varun!</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    color: "cyan",
+    backgroundColor: "#454545",
   },
 });
 
